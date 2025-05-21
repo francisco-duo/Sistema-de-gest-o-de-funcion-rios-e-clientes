7af5c9d6c0002b26147759c5b7b0fc78e9fc18cf
francisco-duo <francisco.duo2000@gmail.com>
2025-05-21 08:17:54 -0300

    feat: Inicializate database with app startup

--- app/api/main.py
+++ app/api/main.py
@@ -1,5 +1,9 @@
 from fastapi import FastAPI
 
+from app.core.database import Base, engine
+
+Base.metadata.create_all(bind=engine)
+
 app: FastAPI = FastAPI(
     title="Sistema de Gestão de Funcionários e Clientes",
     description="API para gerenciar funcionários e clientes",
